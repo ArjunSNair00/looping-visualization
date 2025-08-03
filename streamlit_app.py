d426088b891ffb668368204f060628c7a0aeab85
Arjun S Nair <155005596+ArjunSNair00@users.noreply.github.com>
2025-08-03 18:25:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title("For loop visualization")
+# Slider to control the upper limit of the loop
+limit = st.slider("Loop up to the chosen limit", 1, 20, value=10)
 
-st.write('Hello world!')
+# Loop up to the chosen limit
+for i in range(limit):
+    st.write(f"Value of :blue[***i***]: {i}")
